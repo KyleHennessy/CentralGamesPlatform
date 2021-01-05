a608de644e43640d20f849887d6080ff93e0d6bb
Kyle Hennessy <kyle-hennessy@hotmail.com>
2021-01-05 20:19:45 +0000

    Added a sample game as a proof of concept

--- CentralGamesPlatform/CentralGamesPlatform/Controllers/HomeController.cs
+++ CentralGamesPlatform/CentralGamesPlatform/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace CentralGamesPlatform.Controllers
             return View();
         }
 
+        public IActionResult Play()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
