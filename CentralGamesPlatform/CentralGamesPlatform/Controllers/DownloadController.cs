b2b863a42d3e3e63acabb6197a8a45ff9b7be541
Kyle Hennessy <kyle-hennessy@hotmail.com>
2021-04-15 22:38:18 +0100

    Finished with download game ownership, delivery, creation, update and details

--- CentralGamesPlatform/CentralGamesPlatform/Controllers/DownloadController.cs
+++ CentralGamesPlatform/CentralGamesPlatform/Controllers/DownloadController.cs
@@ -1,23 +1,44 @@
 ﻿using CentralGamesPlatform.IRepositories;
+using CentralGamesPlatform.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace CentralGamesPlatform.Controllers
 {
+    [Authorize]
     public class DownloadController : Controller
     {
         private readonly IDownloadRepository _downloadRepository;
-        public DownloadController(IDownloadRepository downloadRepository)
+        private readonly MyDatabaseContext _myDatabaseContext;
+        public DownloadController(IDownloadRepository downloadRepository, MyDatabaseContext myDatabaseContext)
         {
             _downloadRepository = downloadRepository;
+            _myDatabaseContext = myDatabaseContext;
         }
-        public FileResult DownloadGame(int gameId)
+        public FileResult DownloadGame(int? gameId)
         {
-            var download = _downloadRepository.RetrieveDownloadByGameId(gameId);
-            return File(download.Content, "application/force-download", download.FileName);
+            if (gameId == null)
+            {
+                return null;
+            }
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var query = (from l in _myDatabaseContext.Licences
+                         where l.UserId == userId
+                         join od in _myDatabaseContext.OrderDetails on l.OrderDetailId equals od.OrderDetailId
+                         join g in _myDatabaseContext.Games on od.GameId equals g.GameId
+                         where g.GameId == gameId
+                         select g).SingleOrDefault();
+            if (query == null)
+            {
+                return null;
+            }
+            var download = _downloadRepository.RetrieveDownloadByGameId((int)gameId);
+            return File(download.Content, "application/force-download", download.FileName + download.FileFormat);
         }
     }
 }
